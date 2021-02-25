10a354f7c5ec02c38f60e4ae00fdda040e281591
Burak Bozan <jeagle_1907@hotmail.com>
2021-02-25 20:47:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConferenceScheduler
+ ConferenceScheduler Projects 
---
+++ src/test/java/com/conference/scheduler/ConferenceSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conference.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
